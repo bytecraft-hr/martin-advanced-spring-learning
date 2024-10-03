9ef7e8aeeeece38e83fa5d2ea8f3e84d99847c3f
Miha <mihael@bytecraft.eu>
2024-10-03 17:23:06 +0200

    Cleanup for module3

--- bytecraft/src/main/java/com/example/demo/service/PersonService.java
+++ bytecraft/src/main/java/com/example/demo/service/PersonService.java
@@ -1,14 +1,12 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.Person;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Person;
 
 @Service
 public class PersonService {
@@ -24,36 +22,7 @@ public class PersonService {
 
     public List<Person> getAllPeople() {
         List<Person> people = new ArrayList<>();
-        Connection connection = null;
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(url, user, password);
-
-            String sql = "SELECT * FROM people";
-            PreparedStatement statement = connection.prepareStatement(sql);
-            ResultSet resultSet = statement.executeQuery();
-
-            while (resultSet.next()) {
-                Person person = new Person();
-                person.setPersonId(resultSet.getInt("person_id"));
-                person.setFirstName(resultSet.getString("first_name"));
-                person.setLastName(resultSet.getString("last_name"));
-                person.setEmail(resultSet.getString("email"));
-
-                people.add(person);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (connection != null) connection.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
+        
         return people;
     }
 }
